02c80279b0067f9f9441637c82b5fb4d8182a091
Ismail Ismail <ismail@Ismails-MacBook-Air.local>
2018-01-05 21:48:48 -0800

    added Person, and ClassDataArray for Person

---
+++ Arrays/ClassDataArray.java
@@ -0,0 +1,54 @@
+public class ClassDataArray
+{
+	private Person a[];
+	private int nElems;
+
+	public ClassDataArray(int max) // constructor
+	{
+		a = new Person[max]; // create an array of type Person
+		nElems = 0;
+	}
+
+	public Person fnd(String searchName)
+	{
+		int j;
+		for(j = 0; j < nElems; j++)
+			if(a[j].getLast().equals(searchName)) // found item?
+				break; // exit the loop once found
+		if(j == nElems) // not in array
+			return null;
+		else
+			return a[j];
+	}
+
+	// put Person into an Array
+	public void insert(String last, String first, int age)
+	{
+		a[nElems] = new Person(last, first, age);
+		nElems++; // increament
+	}
+
+	public boolean delete(String searchName)
+	{
+		int j;
+		for(j = 0; j < nElems; j++)
+			if(a[j].getLast().equals(searchName))
+				break;
+		if(j == nElems) // not found
+			return false;
+		else // found it, now delete it, and shift array accordingly
+		{
+			for(int k = j; k > j; k++) // shift down the array
+				a[k] = a[k+1];
+			nElems--; // decreament
+			return true;
+		}
+	}
+
+	public void display()
+	{
+		for(int j = 0; j < nElems; j++)
+			a[j].displayPerson();
+	}
+
+}
